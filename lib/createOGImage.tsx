9c794597f115f94cde2dc447ea07f3060fc01a1c
FrozenHearth <vishudbz@gmail.com>
2023-03-26 16:56:02 +0530

    Update font for og image

--- lib/createOGImage.tsx
+++ lib/createOGImage.tsx
@@ -13,14 +13,14 @@ export const createOgImage = ({
     `w_1600,h_836,q_100`,
 
     // TITLE
-    // Karla google font in light rose
-    `l_text:Karla_72_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
+    // Roboto google font in light rose
+    `l_text:Roboto_72_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_180`,
 
     // META
-    // Karla, but smaller
-    `l_text:Karla_48:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    // Roboto, but smaller
+    `l_text:Roboto_48:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
 
