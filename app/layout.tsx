9872859b389b5bdeaa3dac89160d11b9873c7744
FrozenHearth <vishudbz@gmail.com>
2023-05-08 22:54:12 +0530

    Added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import './global.css';
 import { Providers } from './providers';
+import Footer from '@/components/Footer';
 
 export default function RootLayout({
   children,
@@ -15,7 +16,7 @@ export default function RootLayout({
           <main className="flex-auto min-w-0 md:mt-0 flex flex-col md:pt-12 mx-[6vw] md:mx-[10vw] 2xl:mx-[20vw] justify-center">
             {children}
           </main>
-          <footer className="mt-12 mb-4" />
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="mt-12 bg-slate-50 rounded-lg shadow dark:bg-slate-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
+        <span className="text-md text-gray-500 sm:text-center dark:text-gray-400">
+          Made with Next.js, Tailwind & Contentlayer
+        </span>
+      </div>
+    </footer>
+  );
+}
