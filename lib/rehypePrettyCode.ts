e98a6665a6446dbfd5d7a8450559beea7af10c91
FrozenHearth <vishudbz@gmail.com>
2023-06-17 02:54:37 +0530

    Fix breaking build

--- lib/rehypePrettyCode.ts
+++ lib/rehypePrettyCode.ts
@@ -3,6 +3,6 @@ import { type Options } from 'rehype-pretty-code';
 export const rehypePrettyCodeOptions: Partial<Options> = {
   theme: 'one-dark-pro',
   onVisitHighlightedLine(node) {
-    node.properties.className.push('line--highlighted');
+    node.properties.className?.push('line--highlighted');
   },
 };
