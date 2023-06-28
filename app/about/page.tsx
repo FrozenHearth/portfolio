a5684b4b7f3df0f0e08b7c70d76dd6d754dc3ab4
FrozenHearth <vishudbz@gmail.com>
2023-06-28 17:57:00 +0530

    Fix build error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image';
 import avatar from '@/public/images/me.avif';
 import { createOgImage } from '@/lib/createOGImage';
